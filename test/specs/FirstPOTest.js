ade2fcf8aafd6b099bbeffe752714946985f5043
Fernando1999hsm <fernandocalva15@gmail.com>
2023-02-09 19:31:24 -0600

    Add Credenciatl for Each Test And Add The E2E Test

--- test/specs/FirstPOTest.js
+++ test/specs/FirstPOTest.js
@@ -1,55 +1,62 @@
-import { loginfunction, alert as alertDivNone, signIn as SignInButton, AccesTextInfoP as ParrafTextInfo } from '../pageobjects/loginPage.js'
+import { loginfunction, alert as alertDivNone, signIn as SignInButton, AccesTextInfoP as ParrafTextInfo } from '../pageobjects/loginPage'
 
+import { checkout as CheckOut, cards as CardsP, addProductToCart} from '../pageobjects/shop'
+
+import { productPrices, sumOProducts, totalFormattedPrice } from '../pageobjects/reviewPage'
+
+import { expect as expectChai } from "chai";
 //const logInPage = import "../pageobjects/loginPage.js";
+
+//
+const fs = require('fs')
+let credentials = JSON.parse(fs.readFileSync('test\testData\LoginTest,json'))
+let e2ecredentials = JSON.parse(fs.readFileSync('test\testData\e2eTesting.json'))
 
 describe('Ecommerce Application', async()=>{
 
-    it('Login Fail Page', async()=>{
-        //WebDriverIO  its  ASYNC         await makes SYNC
-        await browser.url("https://rahulshettyacademy.com/loginpagePractise/")
-        console.log(await browser.getTitle())
-        await expect(browser).toHaveTitleContaining("LoginPage Practise | Rahul Shetty Academy")
+    credentials.forEach(({usrname,password})=>{
+        xit('Login Fail Page', async()=>{
+            //WebDriverIO  its  ASYNC         await makes SYNC
+            await browser.url("https://rahulshettyacademy.com/loginpagePractise/")
+            console.log(await browser.getTitle())
+            await expect(browser).toHaveTitleContaining("LoginPage Practise | Rahul Shetty Academy")
 
-        await loginfunction("rahulshettyacademy","learningss")
-        
-        await console.log(await alertDivNone.getText())
-        await browser.waitUntil( async()=> await SignInButton.getAttribute('value') === 'Sign In', 
-        {
-            timeout: 5000,
-            timeoutMsg: 'Error message is not showing up'
+            await loginfunction(usrname,password)
+            
+            await console.log(await alertDivNone.getText())
+            await browser.waitUntil( async()=> await SignInButton.getAttribute('value') === 'Sign In', 
+            {
+                timeout: 5000,
+                timeoutMsg: 'Error message is not showing up'
+            })
+            await console.log(await alertDivNone.getText())
+            await expect(await ParrafTextInfo).toHaveTextContaining("username is rahulshettyacademy and Password is learning")
+            //aqui se perdio todo
         })
-        await console.log(await alertDivNone.getText())
-        await expect(await ParrafTextInfo).toHaveTextContaining("username is rahulshettyacademy and Password is learning")
     })
-
-    xit('Login Success Page', async()=>{
-        await browser.url("https://rahulshettyacademy.com/loginpagePractise/")
-        await $("input[name='username']").setValue("rahulshettyacademy")
-        const password = $("//input[@type='password']")
-        await password.setValue("learning")
-        await $("#signInBtn").click()
-        //wait until this button display
-        await $(".btn-primary").waitForExist()
-        //url
-        await expect(browser).toHaveUrlContaining("shop")
-        await expect(browser).toHaveTitle("ProtoCommerce")
+    e2ecredentials.forEach( ({products}) =>{
+        xit('End to End Test', async()=>{
+            //case from eCommerceE2E.js
+            //const products = ['iphone X', 'Blackberry']
+            await browser.url("https://rahulshettyacademy.com/loginpagePractise/")
+            await loginfunction("rahulshettyacademy","learning")
+            await CheckOut.waitForExist()
+            await addProductToCart(products)
+            await link.click()
+            const sumOfProducts = await sumOProducts()
+            const totalIntegerValue = await totalFormattedPrice()
+    
+            await expectChai(sumOfProducts).to.equal(totalIntegerValue)
+        
+            await $(".btn-success").click()
+            await $("#country").setValue("ind")
+            await $(".lds-ellipsis").waitForExist({reverse:true})
+            //reverse:true makes continue ounce it desapear
+            await $("=India").click()
+            await $("input[type='submit']").click()
+            await expect($(".alert-success")).toHaveTextContaining("Success")
+    
+        })
     })
 
 })
-
-//to run the script:
-//npx wdio run ./wdio.conf.js
-//npx wdio run wdio.conf.js
-
-/* elements from page to webdriver
-id - #id - #username
-class name - .className - .form-control
-
-tragname[attributestag='value'] - - input[name='username']
-
-xpath:
-//tagname[@attribute='value'] -  - //input[@type='password']
-
-parentTag, to childTag
-
-*/
